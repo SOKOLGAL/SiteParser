37d7634c2bbc10407fe3533f268c12c8f5dc7ed3
Sokolova <sokolovagb@gmail.com>
2025-04-12 15:36:05 +0300

    KS-5-8 site parser

--- src/main/kotlin/lesson_5/lesson5_task8.kt
+++ src/main/kotlin/lesson_5/lesson5_task8.kt
@@ -10,8 +10,8 @@ fun main() {
     val doc: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/").get()
     val quotes: Elements = doc.select(".sc-2aegk7-2")
 
-//    println(quotes)
-
-    for (quotes1: Element in quotes) println("${quotes1.text()}\n")
+    for (quotes1: Element in quotes) {
+        println("${quotes1.text()}\n")
+    }
 
 }
